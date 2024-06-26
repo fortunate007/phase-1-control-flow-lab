64f76bf28b23a69e483e8d866f788a842949b066
Newton <newtonkirimi55@gmail.com>
2024-06-26 06:40:51 -0700

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  let greeting;
+
+  if (distanceInFeet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    greeting = 'That will be twenty bucks.';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distanceInFeet > 2500) {
+    greeting = 'No can do.';
+  }
+
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+
